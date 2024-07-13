83421d339821956ab07e1b88e01c2a624e44d4c6
farhad <farhadino@gmail.com>
2024-07-13 22:46:48 +0200

    Update headerNavLinks.ts

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,7 +1,6 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
-  { href: '/blog', title: 'Blog' },
-  { href: '/work', title: 'Work' },
+  { href: '/blog', title: ' },
   { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
 ]
