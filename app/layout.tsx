b44948e2a6ff64025622583c85d3a0c970ffcbbe
Farhad Omid <farhadino@gmail.com>
2025-06-26 00:38:51 +0200

    ttt

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-3TLNT2Q614"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-3TLNT2Q614');
+            `,
+          }}
+        />
+      </head>
       <body
         className={`${geistMono.variable} antialiased font-mono`}
       >
